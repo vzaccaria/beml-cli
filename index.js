838e858a0585c34f77869c62ee6e7e97f1028871
Vittorio Zaccaria <vittorio.zaccaria@gmail.com>
2014-10-27 10:12:08 +0100

    added stdin option

--- index.js
+++ index.js
@@ -4,9 +4,10 @@
   docopt = require('docopt').docopt;
   beml = require('beml');
   fs = require('fs');
-  doc = "Usage:\n  beml-cli <input>\n  beml-cli -h | --help \n";
+  doc = "Usage:\n    beml-cli [<input>]\n    beml-cli -h | --help \n\nOptions:\n    If no <input> is passed, it is read from stdin";
   o = docopt(doc);
   filename = o["<input>"];
+  filename == null && (filename = '/dev/stdin');
   fs.readFile(filename, 'utf-8', function(err, res){
     return console.log(beml.process(res));
   });
--- index.ls
+++ index.ls
@@ -5,13 +5,18 @@ require! 'fs'
 
 doc = """
 Usage:
-  beml-cli <input>
-  beml-cli -h | --help 
+    beml-cli [<input>]
+    beml-cli -h | --help 
 
+Options:
+    If no <input> is passed, it is read from stdin
 """
 
 o = docopt(doc)
+
 filename = o["<input>"]
+filename ?= '/dev/stdin'
+
 fs.readFile filename, 'utf-8', (err, res) ->
     console.log beml.process(res)
 
